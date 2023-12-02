b18f6f552124e1b64aac0fefb2430275a3dee0b6
nrr <nickrrau@gmail.com>
2023-12-01 23:40:27 -0500

    Minor template adjustments to support two parts

--- build.zig
+++ build.zig
@@ -13,7 +13,22 @@ const DayScaffoldTemplate =
     \\const std = @import("std");
     \\const source = @import("days/day_{d}/day_{d}_input.zig");
     \\
-    \\pub fn process(alloc: std.mem.Allocator) !void {{
+    \\pub fn processPartOne(alloc: std.mem.Allocator) !void {{
+    \\    var stream = std.io.fixedBufferStream(source.input_sample);
+    \\    var reader = stream.reader();
+    \\    var process_buffer = std.ArrayList(u8).init(alloc);
+    \\    var buffer_writer = process_buffer.writer();
+    \\    while (reader.streamUntilDelimiter(buffer_writer, '\n', null)) |line| {{
+    \\        _ = line;
+    \\        // Process input here
+    \\    }} else |err| switch (err) {{
+    \\        error.EndOfStream => {{}},
+    \\        else => return err,
+    \\    }}
+    \\}}
+    \\
+    \\
+    \\pub fn processPartTwo(alloc: std.mem.Allocator) !void {{
     \\    var stream = std.io.fixedBufferStream(source.input_sample);
     \\    var reader = stream.reader();
     \\    var process_buffer = std.ArrayList(u8).init(alloc);
@@ -29,9 +44,16 @@ const DayScaffoldTemplate =
     \\
     \\pub fn main() !void {{
     \\    var allocator = std.heap.HeapAllocator.init();
-    \\    var arena = std.heap.ArenaAllocator.init(allocator.allocator());
-    \\    defer arena.deinit();
-    \\    return try process(arena.allocator());
+    \\    {{
+    \\        var arena = std.heap.ArenaAllocator.init(allocator.allocator());
+    \\        defer arena.deinit();
+    \\        try processPartOne(arena.allocator());
+    \\    }}
+    \\    {{
+    \\        var arena = std.heap.ArenaAllocator.init(allocator.allocator());
+    \\        defer arena.deinit();
+    \\        try processPartTwo(arena.allocator());
+    \\    }}
     \\}}
 ;
 
--- src/day_1.zig
+++ src/day_1.zig
@@ -1,7 +1,21 @@
 const std = @import("std");
 const source = @import("days/day_1/day_1_input.zig");
 
-pub fn process(alloc: std.mem.Allocator) !void {
+pub fn processPartOne(alloc: std.mem.Allocator) !void {
+    var stream = std.io.fixedBufferStream(source.input_sample);
+    var reader = stream.reader();
+    var process_buffer = std.ArrayList(u8).init(alloc);
+    var buffer_writer = process_buffer.writer();
+    while (reader.streamUntilDelimiter(buffer_writer, '\n', null)) |line| {
+        _ = line;
+        // Process input here
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    }
+}
+
+pub fn processPartTwo(alloc: std.mem.Allocator) !void {
     var stream = std.io.fixedBufferStream(source.input_sample);
     var reader = stream.reader();
     var process_buffer = std.ArrayList(u8).init(alloc);
@@ -17,7 +31,14 @@ pub fn process(alloc: std.mem.Allocator) !void {
 
 pub fn main() !void {
     var allocator = std.heap.HeapAllocator.init();
-    var arena = std.heap.ArenaAllocator.init(allocator.allocator());
-    defer arena.deinit();
-    return try process(arena.allocator());
+    {
+        var arena = std.heap.ArenaAllocator.init(allocator.allocator());
+        defer arena.deinit();
+        try processPartOne(arena.allocator());
+    }
+    {
+        var arena = std.heap.ArenaAllocator.init(allocator.allocator());
+        defer arena.deinit();
+        try processPartTwo(arena.allocator());
+    }
 }
